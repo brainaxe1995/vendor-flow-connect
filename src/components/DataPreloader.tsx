f97473a8fac12238cb21617feaaf2c6465ba23e2
brainaxe1995 <108518951+brainaxe1995@users.noreply.github.com>
2025-06-13 01:21:25 +0400

    feat: preload WooCommerce and Supabase data

---
+++ src/components/DataPreloader.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import { useDataPreloader } from '@/hooks/useDataPreloader';
+
+const DataPreloader = () => {
+  useDataPreloader();
+  return null;
+};
+
+export default DataPreloader;
